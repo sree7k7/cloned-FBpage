1ba07ed8af9cf59d476f03b425db3029ff3db4e3
Srikanth <sree7k7@gmail.com>
2020-01-23 16:12:52 +0100

    fistfb

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <hearder></hearder>
+    <leftbody></leftbody>
+    <rightbody></rightbody>
+    <footercomponent></footercomponent>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import hearder from "./components/header.vue";
+import leftbody from "./components/leftbody.vue";
+import rightbody from "./components/rightbody.vue";
+import footercomponent from "./components/footercomponent.vue";
+export default {
+  components: {
+    hearder,
+    leftbody,
+    rightbody,
+    footercomponent
   }
-}
+};
+</script>
+<style >
 </style>
